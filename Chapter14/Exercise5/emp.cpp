37a70067b20ce441497153963168507ed9b74143
jukanntenn <jukanntenn@outlook.com>
2022-03-06 21:05:56 +0800

    finished Chapter14::Exercise5

---
+++ Chapter14/Exercise5/emp.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include "emp.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+abstr_emp::abstr_emp() {}
+abstr_emp::abstr_emp(
+    const std::string &fn,
+    const std::string &ln,
+    const std::string &j) : fname(fn), lname(ln), job(j)
+{
+}
+
+void abstr_emp::ShowAll() const
+{
+    cout << "First name: " << fname << endl;
+    cout << "Last name: " << lname << endl;
+    cout << "Job: " << job << endl;
+}
+
+void abstr_emp::SetAll()
+{
+    cout << "Enter first name: ";
+    getline(cin, fname);
+
+    cout << "Enter last name: ";
+    getline(cin, lname);
+
+    cout << "Enter job: ";
+    getline(cin, job);
+}
+
+std::ostream &operator<<(std::ostream &os, const abstr_emp &e)
+{
+    os << e.fname << " " << e.lname;
+    return os;
+}
+
+abstr_emp::~abstr_emp() {}
+
+employee::employee() : abstr_emp()
+{
+}
+
+employee::employee(const std::string &fn,
+                   const std::string &ln,
+                   const std::string &j) : abstr_emp(fn, ln, j)
+{
+}
+
+void employee::ShowAll() const
+{
+    abstr_emp::ShowAll();
+    cout << endl;
+}
+
+void employee::SetAll()
+{
+    abstr_emp::SetAll();
+}
+
+manager::manager() : abstr_emp()
+{
+}
+
+manager::manager(const std::string &fn, const std::string &ln,
+                 const std::string &j, int ico) : abstr_emp(fn, ln, j), inchargeof(ico)
+{
+}
+
+manager::manager(const abstr_emp &e, int ico) : abstr_emp(e), inchargeof(ico)
+{
+}
+
+manager::manager(const manager &m) : abstr_emp(m)
+{
+    inchargeof = m.InChargeOf();
+}
+
+void manager::ShowAll() const
+{
+    abstr_emp::ShowAll();
+    cout << "In charge of: " << inchargeof << endl
+         << endl;
+}
+
+void manager::SetAll()
+{
+    abstr_emp::SetAll();
+    cout << "Enter in charge of: ";
+    cin >> inchargeof;
+}
+
+fink::fink() : abstr_emp()
+{
+}
+
+fink::fink(const std::string &fn, const std::string ln,
+           const std::string &j, const std::string rpo) : abstr_emp(fn, ln, j), reportsto(rpo)
+{
+}
+
+fink::fink(const abstr_emp &e, const std::string &rpo) : abstr_emp(e), reportsto(rpo)
+{
+}
+
+fink::fink(const fink &f) : abstr_emp(f)
+{
+    reportsto = f.reportsto;
+}
+
+void fink::ShowAll() const
+{
+    abstr_emp::ShowAll();
+    cout << "Reports to: " << reportsto << endl
+         << endl;
+}
+
+void fink::SetAll()
+{
+    abstr_emp::SetAll();
+    cout << "Enter reports to: ";
+    getline(cin, reportsto);
+}
+
+highfink::highfink() : abstr_emp(), manager(), fink()
+{
+}
+
+highfink::highfink(const std::string &fn, const std::string &ln,
+                   const std::string &j, const std::string &rpo,
+                   int ico) : abstr_emp(fn, ln, j), manager(fn, ln, j, ico), fink(fn, ln, j, rpo)
+{
+}
+
+highfink::highfink(const abstr_emp &e, const std::string rpo, int ico) : abstr_emp(e), manager(e, ico), fink(e, rpo)
+{
+}
+
+highfink::highfink(const fink &f, int ico) : abstr_emp(f), manager(f, ico), fink(f)
+{
+}
+
+highfink::highfink(const manager &m, const std::string rpo) : abstr_emp(m), manager(m)
+{
+    ReportsTo() = rpo;
+}
+
+highfink::highfink(const highfink &h) : abstr_emp(h), manager(h), fink(h)
+{
+}
+
+void highfink::ShowAll() const
+{
+    abstr_emp::ShowAll();
+    cout << "In charge of: " << InChargeOf() << endl;
+    cout << "Reports to: " << ReportsTo() << endl
+         << endl;
+}
+
+void highfink::SetAll()
+{
+    abstr_emp::SetAll();
+    cout << "Enter in charge of: ";
+    int tmp;
+    cin >> InChargeOf();
+    cin.get();
+    cout << "Enter reports to: ";
+    getline(cin, ReportsTo());
+}
